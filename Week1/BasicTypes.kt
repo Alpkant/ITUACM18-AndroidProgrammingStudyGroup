a7711839c49d0cd9b92bafa58a5114c08f49ab3e
Alperen Kantarcı <kantarcia@itu.edu.tr>
2018-10-18 16:04:30 +0300

    Update BasicTypes.kt

--- Week1/BasicTypes.kt
+++ Week1/BasicTypes.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>): Unit {
     /* Val - Var difference */
 
     val val1 = 123
-    // val1 = 246 <- Val value cannot be assigned
+    // val1 = 246 <- Val value cannot be reassigned
     var var1 = 123
     println(var1)
     var1 = 256
@@ -11,12 +11,12 @@ fun main(args: Array<String>): Unit {
 
     /* Basic Data Types */
 
-    val a: Int = 100 //32 bit
-    val d: Double = 100.00 //64 bit
-    val f: Float = 100.00f //32 bit
-    val l: Long = 2000000004 //64 bit
-    val s: Short = 10 //16 bit
-    val b: Byte = 1 // 8
+    val a: Int = 100 //32 bits
+    val d: Double = 100.00 //64 bits
+    val f: Float = 100.00f //32 bits
+    val l: Long = 2000000004 //64 bits
+    val s: Short = 10 //16 bits
+    val b: Byte = 1 // 8 bits
 
 
     /* Strings and prints */
@@ -24,7 +24,7 @@ fun main(args: Array<String>): Unit {
     val letter: Char
     letter = 'A'
     val var2 = "Long string"
-    print(letter) // Print function doesn't provide automatic end of line
+    print(letter) // print function doesn't provide automatic end of line
     print(letter + "\n")
     println(var2)
 
